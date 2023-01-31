1aafbee38974b7a28b6cfb3d1f77771ec2a8b4be
Osgoroth <david-lucas-social@outlook.com>
2023-01-31 09:55:43 +0800

    set up npm and added hammer and dexie dep.

--- modules/recipes.mjs
+++ modules/recipes.mjs
@@ -1,3 +1,5 @@
+include 
+
 db.open().catch(function (err) {
   alert("Failed to open db: " + (err.stack || err));
 });
@@ -12,12 +14,38 @@ function loadRecipes() {
     .toArray()
     .then((recipes) => {
       recipes.forEach((recipe) => {
-        const recipeCard = /*html*/ `
-        <div class="col" onclick=viewRecipe(${recipe.id})>
-          <div class="card h-100 shadow-sm recipe-card ">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="125px" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="d-block text-truncate card-text py-0">${recipe.name}</p>
+        const recipeCard = /*html*/ ` 
+        <div
+          class="col"
+          
+        >
+          <div class="card h-100 shadow-sm recipe-card">
+            <div>
+              <div class="btn-group position-absolute top-0 end-0 px-1 z-1">
+                <button class="btn btn-link btn-sm link-secondary" onclick=""><i class="bi bi-pencil-square"></i></button>
+                <button class="btn btn-link btn-sm link-secondary" onclick="deleteRecipe(${recipe.id})"><i class="bi bi-x-square"></i></button>
+              </div>
+            </div>
+            <div onclick="viewRecipe(${recipe.id})">
+              <svg
+                class="bd-placeholder-img card-img-top"
+                width="100%"
+                height="125px"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: Thumbnail"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#55595c" />
+                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+              </svg>
+              <div class="card-body">
+                <p class="d-inline-block text-truncate card-text py-0">
+                  ${recipe.name}
+                </p>
+              </div>
             </div>
           </div>
         </div>`;
@@ -48,7 +76,7 @@ function viewRecipe(id) {
         ".floating-button-back"
       );
 
-      let recipeInfo = /*html*/ `<div class="row"><h1>${recipe.name}</h1></div>
+      let recipeInfo = `<div class="row"><h1>${recipe.name}</h1></div>
         <div class="row"><p>${recipe.description}</p></div>`;
       infoContainer.insertAdjacentHTML("beforeend", recipeInfo);
 
@@ -58,11 +86,11 @@ function viewRecipe(id) {
       recipe.ingredients.forEach(
         (ingredient) =>
           (ingredientRows += /*html*/ `
-            <tr>
-              <td>${ingredient[0]} ${ingredient[1]} </td>
-              
-              <td>${ingredient[2]}</td>
-            </tr>`)
+              <tr>
+                <td>${ingredient[0]} ${ingredient[1]}</td>
+                <td>${ingredient[2]}</td>
+              </tr>
+            `)
       );
       ingredientsTableBody.insertAdjacentHTML("beforeend", ingredientRows);
 
@@ -75,7 +103,6 @@ function viewRecipe(id) {
             <tr>
               <td>${step[0]}:</td>
               <td>${step[1]}</td>
-              
             </tr>`)
       );
       methodTableBody.insertAdjacentHTML("beforeend", methodRows);
@@ -94,4 +121,10 @@ function toggleVisibility(...classNames) {
       element.classList.add("visually-hidden");
     }
   }
+}
+
+function deleteRecipe(recipeID) {
+  db.recipes.delete(recipeID);
+  //reload recipes
+  loadRecipes();
 }
